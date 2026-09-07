199f1743cb039792f46c7bba3872ef10ae9f08de
practiseo <practiseo@gmail.com>
2026-09-07 15:23:33 +0200

    refactor: remove unused components and hooks to streamline codebase

--- src/discovery/interfaceRegistry.ts
+++
@@ -1,6 +0,0 @@
-import { AsyncComponentRegistry } from './registry';
-import type { InterfaceMeta } from './registryTypes';
-
-class InterfaceRegistry extends AsyncComponentRegistry<InterfaceMeta> {}
-
-export const interfaceRegistry = new InterfaceRegistry();
