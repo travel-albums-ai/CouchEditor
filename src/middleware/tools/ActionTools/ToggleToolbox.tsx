ee70d206ac608710c72fbffa141b8291a83b61c2
practiseo <practiseo@gmail.com>
2026-09-07 18:39:40 +0200

    style: remove unused imports and commented code in ToggleToolbox for cleaner implementation

--- src/middleware/tools/ActionTools/ToggleToolbox.tsx
+++ src/middleware/tools/ActionTools/ToggleToolbox.tsx
@@ -2,18 +2,10 @@ import { GenericToggleButtonProps } from '@/components/generics/GenericToggleBut
 import GenericToggleButtonGroup from '@/components/generics/GenericToggleButtonGroup';
 import { usePipelineStore, usePipelineStoreSelector } from '@/context/pipelineStore';
 import { PanelLeftDashed } from 'lucide-react';
-import { useTranslation } from 'react-i18next';
 
 export default function ToggleToolbox() {
-  // const { setSetting } = useSettings()
-  // const themeMode = useSettingsStoreSelector((state) => state.);
   const showToolbox = usePipelineStoreSelector(state => state.showToolbox);
   const { toggleToolbox } = usePipelineStore();
-  const { t } = useTranslation()
-
-  // const handleOnChange = (mode?: 'light' | 'dark') => {
-  //   setSetting((prev) => ({ ...prev, themeMode: mode === 'light' ? 'dark' : 'light'}))
-  // }
 
   return <>
     <GenericToggleButtonGroup variant="standard" id="toggle-toolbox-toggle" items={[
