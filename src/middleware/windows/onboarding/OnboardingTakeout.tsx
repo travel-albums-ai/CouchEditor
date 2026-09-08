f8bc097c2198bd595cce51aaac5592730120b4a7
practiseo <practiseo@gmail.com>
2026-09-08 10:53:56 +0200

    refactor: simplify OnboardingTakeout component by removing unnecessary wrappers and updating phase steps

--- src/middleware/windows/onboarding/OnboardingTakeout.tsx
+++ src/middleware/windows/onboarding/OnboardingTakeout.tsx
@@ -1,56 +1,29 @@
-import OnboardingButton from '@/middleware/windows/onboarding/OnboardingButton';
-import OnboardingPhasesList from '@/middleware/windows/onboarding/OnboardingPhasesList';
-import OnboardingWrapper from '@/middleware/windows/onboarding/OnboardingWrapper';
-import OnboardingWrapperInfo from '@/middleware/windows/onboarding/OnboardingWrapperInfo';
+import OnboardingPhasesListHorizontal from '@/middleware/windows/onboarding/OnboardingPhasesListHorizontal';
+import LayoutPopover from '@/middleware/windows/settings/LayoutPopover';
+import { Box } from '@mui/material';
 import { Astroid, Turtle } from 'lucide-react';
 
 const phaseSteps = [
   {
     key: '1',
     icon: <Astroid />,
-    titleKey: 'AI as BYOK',
-    descriptionKey: 'Add AI features to your pipeline to enhance the capabilities.',
+    titleKey: 'I probably speak your language',
+    descriptionKey: 'Pick your preferred language.',
   },
   {
     key: '2',
     icon: <Turtle />,
-    titleKey: "High Performance",
-    descriptionKey: 'Achieve high performance in your pipelines with optimized configurations. Benefits include faster execution and efficient resource utilization.',
+    titleKey: "Theme Preference",
+    descriptionKey: 'Choose between light and dark themes for the application interface.',
   },
 ]
 
 export default function OnboardingTakeout() {
 
   return (<>
-    <OnboardingWrapper>
-      <div
-        style={{
-          position: "relative",
-          width: "100%",
-          aspectRatio: "16 / 9",
-          overflow: "hidden",
-          borderRadius: 4,
-        }}
-      >
-        <iframe
-          src="https://www.youtube-nocookie.com/embed/nTLMbpX_4hQ"
-          title="YouTube video"
-          style={{
-            position: "absolute",
-            backgroundColor: 'transparent',
-            inset: 0,
-            width: "100%",
-            height: "100%",
-            border: 0,
-          }}
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        />
-      </div>
-      <OnboardingButton href="https://takeout.google.com/settings/takeout" label={'Open Google Takeout...'} />
-    </OnboardingWrapper>
-    <OnboardingWrapperInfo light={true}>
-      <OnboardingPhasesList phaseSteps={phaseSteps} />
-    </OnboardingWrapperInfo>
+    <Box sx={{ display: 'flex', justifyContent: 'stretch', mb: 2, flexDirection: 'column', p: 2 }}>
+      <LayoutPopover />
+    </Box>
+    <OnboardingPhasesListHorizontal phaseSteps={phaseSteps} />
   </>)
 }
