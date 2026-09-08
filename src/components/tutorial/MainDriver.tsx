1a2260d803e693082a197b4ec25c45d25e04eb2d
practiseo <practiseo@gmail.com>
2026-09-08 11:11:07 +0200

    refactor: streamline MainDriver component by consolidating tutorial steps and removing unused code

--- src/components/tutorial/MainDriver.tsx
+++ src/components/tutorial/MainDriver.tsx
@@ -1,96 +1,8 @@
+import { useSettings, useSettingsStoreSelector } from '@/context/settingsStore';
+import { driver, DriveStep } from "driver.js";
 import { useEffect } from 'react';
 
-import { useSettings, useSettingsStoreSelector } from '@/context/settingsStore';
-import { driver } from "driver.js";
-
-
-// const sidebar = [
-//   {
-//     element: "#sidebar",
-//     popover: {
-//       title: "Discover all sections",
-//       description: "Your photos are organized in sections. You can also pin your favorite sections here.",
-//       side: "left",
-//     },
-//   },
-//   {
-//     element: "#sort-sections-toggle",
-//     popover: {
-//       title: "Sort Sections",
-//       description: "Use this toggle to sort sections in ascending or descending order.",
-//       side: "bottom",
-//     },
-//   },
-//   {
-//     element: "#sidebar-section-peopleAndPets",
-//     popover: {
-//       title: "People & Pets",
-//       description: "Discover your friends and pets in your photos.",
-//       side: "bottom",
-//     },
-//   },
-//   {
-//     element: "#sidebar-section-folders",
-//     popover: {
-//       title: "Folders",
-//       description: "Find what what was indexed in your folders.",
-//       side: "bottom",
-//     },
-//   },
-//   {
-//     element: "#sidebar-section-countries",
-//     popover: {
-//       title: "Countries",
-//       description: "Discover the countries where your photos were taken.",
-//       side: "bottom",
-//     },
-//   }
-// ]
-
-// const statusBar = [
-//   {
-//     element: "#status-bar",
-//     popover: {
-//       title: "Status Bar",
-//       description: "This bar displays the current status of your application, including any notifications or messages.",
-//       side: "top",
-//     },
-//   },
-//   {
-//     element: "#database-counts",
-//     popover: {
-//       title: "Database Counts",
-//       description: "This section displays the counts of various items in your database.",
-//       side: "top",
-//     },
-//   },
-//   {
-//     element: "#zoom-controls",
-//     popover: {
-//       title: "Zoom controls",
-//       description: "Use these controls to zoom in and out of your photos.",
-//       side: "top",
-//     },
-//   },
-//   {
-//     element: "#indexer",
-//     popover: {
-//       title: "Indexer",
-//       description: "Use the indexer to process your photos efficiently.",
-//       side: "top",
-//     },
-//   },
-//   {
-//     element: "#keyboard-list",
-//     popover: {
-//       title: "Be efficient with shortcuts",
-//       description: "Consult the list of keyboard shortcuts available in the app. Dynamically updated based on your current context.",
-//       side: "top",
-//     },
-//   },
-// ]
-
-const header = [
+const steps = [
   {
     element: "#pipeline-header-left",
     popover: {
@@ -164,23 +76,6 @@ const header = [
     },
   },
   {
-    element: "#pipeline-toolbox",
-    popover: {
-      title: "Pipeline Toolbox",
-      description: "Use this toolbox to access various pipeline tools and functionalities.",
-      side: "bottom",
-    },
-  },
-  {
-    element: "#pipeline-trash",
-    popover: {
-      title: "Pipeline Trash",
-      description: "Use this toggle to open and close the pipeline trash.",
-      side: "bottom",
-    },
-  },
-
-  {
     element: "#fullscreen-toggle",
     popover: {
       title: "Fullscreen Toggle",
@@ -196,7 +91,31 @@ const header = [
       side: "bottom",
     },
   },
-]
+  {
+    element: "#pipeline-toolbox",
+    popover: {
+      title: "Pipeline Toolbox",
+      description: "Use this toolbox to access various pipeline tools and functionalities.",
+      side: "bottom",
+    },
+  },
+  {
+    element: "#pipeline-trash",
+    popover: {
+      title: "Pipeline Trash",
+      description: "Use this toggle to open and close the pipeline trash.",
+      side: "bottom",
+    },
+  },
+  {
+    element: "#outlet-drawer",
+    popover: {
+      title: "Main Screen",
+      description: "This is where your main content is displayed. Psst: is has draggable items",
+      side: "top",
+    },
+  },
+] as DriveStep[]
 
 export default function MainDriver() {
   const { setSetting } = useSettings()
@@ -208,35 +127,7 @@ export default function MainDriver() {
       animate: true,
       overlayColor: "rgba(0,0,0,0.7)",
 
-      steps: [
-        ...header,
-        // ...sidebar,
-        // ...statusBar,
-        {
-          element: "#outlet-drawer",
-          popover: {
-            title: "Outlet Drawer",
-            description: "This is where your main content is displayed. Psst: is has draggable tabs",
-            side: "top",
-          },
-        },
-        {
-          element: ".flexlayout__tabset_tabbar_inner",
-          popover: {
-            title: "Different views",
-            description: "Switch between different views of your content using these tabs.",
-            side: "top",
-          },
-        },
-        {
-          element: "#photo-drawer",
-          popover: {
-            title: "Photo Drawer",
-            description: "This drawer contains your photo content and image details.",
-            side: "top",
-          },
-        },
-      ],
+      steps,
       onDestroyed: () => {
         setSetting((prev) => ({
           ...prev,
