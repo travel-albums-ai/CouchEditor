ac9d273e4e7c4afa5692c92711d777e75e9842a6
practiseo <practiseo@gmail.com>
2026-09-10 12:13:50 +0200

    feat: enhance SinglePhotoViewerNode layout and styling with Typography and theme integration

--- src/middleware/windows/pipeline/ComplexNodes/SinglePhotoViewerNode.tsx
+++ src/middleware/windows/pipeline/ComplexNodes/SinglePhotoViewerNode.tsx
@@ -3,7 +3,7 @@ import SolidChip from '@/components/SolidChip';
 import { useSettingsStoreSelector } from '@/context/settingsStore';
 import { InputHandle } from '@/middleware/windows/pipeline/components/InputHandle';
 import NodeWrapper from '@/middleware/windows/pipeline/components/NodeWrapper';
-import { Box } from '@mui/material';
+import { Box, Typography, useTheme } from '@mui/material';
 import { Position, type Node, type NodeProps } from "@xyflow/react";
 import { HardDrive, RulerDimensionLine } from 'lucide-react';
 import type { ImageArray } from "../types";
@@ -16,6 +16,7 @@ function SinglePhotoViewerNode({
   data,
 }: NodeProps<Node<{ image?: ImageArray }>>) {
   const previewPhotoObj = useSettingsStoreSelector((state) => state.previewPhotoObj);
+  const theme = useTheme();
 
   const images = data.image ?? [];
   const match = previewPhotoObj
@@ -28,14 +29,19 @@ function SinglePhotoViewerNode({
       <Box sx={{ height: '600px', width: '600px', overflow: 'auto' }}>
         {match ? (
           <>
-            <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', gap: 2, pb: 1  }}>
-              <SolidChip label={`${match.width} x ${match.height} px`} fontSize={14} minWidth={120} height={28} icon={<RulerDimensionLine />} />
-              <SolidChip label={`${formatMegabytes(match.byteSize)}`} fontSize={14} minWidth={100} height={28} icon={<HardDrive />} />
+            <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', gap: 2, pb: 1  }}>
+              <Typography variant="body2" noWrap sx={{ flex: 1, fontWeight: 'bold' }}>
+                {match.name}
+              </Typography>
+              <SolidChip label={`${match.width} x ${match.height} px`} fontSize={14} minWidth={32} height={28} icon={<RulerDimensionLine />} />
+              <SolidChip label={`${formatMegabytes(match.byteSize)}`} fontSize={14} minWidth={32} height={28} icon={<HardDrive />} />
             </Box>
             <img
               src={match.src}
               alt={match.name ?? ''}
               style={{
+                border: '1px dotted',
+                borderColor: theme.palette.divider,
                 display: 'block',
                 width: '100%',
                 height: '550px',
