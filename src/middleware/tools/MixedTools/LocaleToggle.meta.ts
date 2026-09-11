f5c1c3c6166e7c687be3765d0960738b6476a84e
practiseo <practiseo@gmail.com>
2026-09-11 16:04:27 +0200

    refactor: remove deprecated tool discovery and registry components

--- src/middleware/tools/MixedTools/LocaleToggle.meta.ts
+++
@@ -1,9 +0,0 @@
-import { ToolMeta } from '@/discovery/registryTypes';
-
-export const meta = {
-  id: "localeToggle",
-  tool: [
-
-  ],
-  loader: () => import('@/middleware/tools/MixedTools/LocaleToggle'),
-} as ToolMeta;
