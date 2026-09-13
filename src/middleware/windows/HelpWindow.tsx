18b07acb804935b45172f3fcb3693691f3bb9552
practiseo <practiseo@gmail.com>
2026-09-13 23:52:33 +0200

    feat: update HelpWindow to use grouped palette items and enhance layout

--- src/middleware/windows/HelpWindow.tsx
+++ src/middleware/windows/HelpWindow.tsx
@@ -1,13 +1,14 @@
 import { useSettings, useSettingsStoreSelector } from '@/context/settingsStore';
 import NodeToolboxHeader from '@/middleware/windows/pipeline/components/NodeToolboxHeader';
 import PreviewDemo from '@/middleware/windows/pipeline/components/PreviewDemo';
-import { paletteItems } from '@/middleware/windows/pipeline/NodePalette';
-import { Alert, Box, Dialog } from '@mui/material';
+import { groupedPaletteItems } from '@/middleware/windows/pipeline/NodePalette';
+import { Alert, Box, Dialog, Divider, Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
 export default function HelpWindow() {
   const showHelp = useSettingsStoreSelector(s => s.showHelp)
   const { setSetting } = useSettings()
-
+  const { t } = useTranslation();
   const showWindow = showHelp === true
 
   if (!showWindow) return null
@@ -34,29 +35,41 @@ export default function HelpWindow() {
         Discover how to use the different nodes in the pipeline below.
       </Alert>
 
-      <Box sx={{
-        pt: 4,
-        display: 'grid',
-        gridTemplateColumns: 'repeat(2, 1fr)',
-        gap: 2,
-      }}>
-        {paletteItems.map(paletteItem => (<Box key={paletteItem.type} sx={{ display: 'flex', flexDirection: 'column' }}>
-          <NodeToolboxHeader type={paletteItem.type}></NodeToolboxHeader>
+      {Object.entries(groupedPaletteItems).map(([groupName, group]) => (
+        <Box key={groupName} sx={{ display: 'flex', flexDirection: 'column' }}>
+
+          <Divider sx={{ my: 2, textTransform: 'capitalize', borderStyle: 'dotted' }}>
+            <Typography color="textSecondary" variant="caption">{t(groupName)}</Typography>
+          </Divider>
+
           <Box sx={{
-            boxShadow: theme => `inset 0 0 8px 0px ${theme.palette.divider}`,
-            borderRadius: 2,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            // m: 1,
-            py: 2 }}>
-            <PreviewDemo paletteItem={paletteItem} />
-            <NodeToolboxHeader type={paletteItem.type}></NodeToolboxHeader>
-            {/* <h3>{paletteItem.name}</h3>
-            <p>{paletteItem.description}</p> */}
+            display: 'grid',
+            alignSelf: 'stretch',
+
+            gridTemplateColumns: 'repeat(3, 1fr)',
+            gap: 2,
+            mb: 4,
+          }}>
+            {group.map(paletteItem => (
+              <Box key={paletteItem.type} sx={{ display: 'flex', flexDirection: 'column' }}>
+                <NodeToolboxHeader type={paletteItem.type}></NodeToolboxHeader>
+                <Box sx={{
+                  boxShadow: theme => `inset 0 0 8px 0px ${theme.palette.divider}`,
+                  borderRadius: 2,
+                  display: 'flex',
+                  flex: 1,
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  py: 2 }}>
+                  <PreviewDemo paletteItem={paletteItem} />
+                  {/* <NodeToolboxHeader type={paletteItem.type}></NodeToolboxHeader> */}
+                </Box>
+              </Box>
+            ))}
           </Box>
-        </Box>))}
-      </Box>
+        </Box>
+      ))}
     </Dialog>
   )
 }
