70d9d4ac16f028d3574fe30f8fc72c1385e238a5
practiseo <practiseo@gmail.com>
2026-09-13 23:39:57 +0200

    feat: enhance HelpWindow with informative alerts and improved layout

--- src/middleware/windows/HelpWindow.tsx
+++ src/middleware/windows/HelpWindow.tsx
@@ -1,6 +1,8 @@
 import { useSettings, useSettingsStoreSelector } from '@/context/settingsStore';
+import NodeToolboxHeader from '@/middleware/windows/pipeline/components/NodeToolboxHeader';
+import PreviewDemo from '@/middleware/windows/pipeline/components/PreviewDemo';
 import { paletteItems } from '@/middleware/windows/pipeline/NodePalette';
-import { Dialog } from '@mui/material';
+import { Alert, Box, Dialog } from '@mui/material';
 
 export default function HelpWindow() {
   const showHelp = useSettingsStoreSelector(s => s.showHelp)
@@ -28,13 +30,33 @@ export default function HelpWindow() {
       }}
     >
       <div>Help Content</div>
-      {paletteItems.map(paletteItem => (
-        <div>
-          sss
-          <h3>{paletteItem.name}</h3>
-          <p>{paletteItem.description}</p>
-        </div>
-      ))}
+      <Alert severity="info">
+        Discover how to use the different nodes in the pipeline below.
+      </Alert>
+
+      <Box sx={{
+        pt: 4,
+        display: 'grid',
+        gridTemplateColumns: 'repeat(2, 1fr)',
+        gap: 2,
+      }}>
+        {paletteItems.map(paletteItem => (<Box key={paletteItem.type} sx={{ display: 'flex', flexDirection: 'column' }}>
+          <NodeToolboxHeader type={paletteItem.type}></NodeToolboxHeader>
+          <Box sx={{
+            boxShadow: theme => `inset 0 0 8px 0px ${theme.palette.divider}`,
+            borderRadius: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            // m: 1,
+            py: 2 }}>
+            <PreviewDemo paletteItem={paletteItem} />
+            <NodeToolboxHeader type={paletteItem.type}></NodeToolboxHeader>
+            {/* <h3>{paletteItem.name}</h3>
+            <p>{paletteItem.description}</p> */}
+          </Box>
+        </Box>))}
+      </Box>
     </Dialog>
   )
 }
