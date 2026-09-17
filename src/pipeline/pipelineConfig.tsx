e676cea97363b2056d20e49e4b6e1b83a55e55ed
practiseo <practiseo@gmail.com>
2026-09-17 19:30:54 +0200

    feat: Add new complex nodes for image processing pipeline
    - Introduced InformationNode for displaying and editing text content with different sizes.
    - Added LutNode for uploading LUT files with a user-friendly interface.
    - Implemented MergeChannelsNode to merge color channels from images.
    - Created PhotoHistogramNode to visualize histograms of selected photos.
    - Developed RescaleNode for adjusting image scale with preset options.
    - Added SelectedPhotoNode to display the currently selected photo.
    - Implemented SinglePhotoViewerNode for viewing individual photos with details.
    - Created SourceNode for importing images with drag-and-drop support.
    - Added SplitChannelsNode to split images into their respective color channels.
    - Developed SplitToningNode for applying split toning effects to images.
    - Created ToneNodeLayout for consistent layout across tone adjustment nodes.
    - Implemented ViewerNode for displaying multiple images with download functionality.
    - Added VignetteNode for applying vignette effects with adjustable parameters.
    - Updated index file to export new nodes for use in the pipeline.

--- src/pipeline/pipelineConfig.tsx
+++ src/pipeline/pipelineConfig.tsx
@@ -1,41 +1,41 @@
 /* eslint-disable react-refresh/only-export-components */
 import { ConnectionLineType, type Edge, type Node } from '@xyflow/react';
 
-import AIAsyncColorizerNode from '@/node/ComplexNodes/AIAsyncColorizerNode';
-import AIAsyncDenoiserNode from '@/node/ComplexNodes/AIAsyncDenoiserNode';
-import AIPhotoEditorNode from '@/node/ComplexNodes/AIPhotoEditorNode';
-import ArraySetOperationNode from '@/node/ComplexNodes/ArraySetOperationNode';
-import ArraySwitchNode from '@/node/ComplexNodes/ArraySwitchNode';
-import AskAINode from '@/node/ComplexNodes/AskAINode';
-import CollageNode from '@/node/ComplexNodes/CollageNode';
-import ExifSplitNode from '@/node/ComplexNodes/ExifSplitNode';
-import ExifViewerNode from '@/node/ComplexNodes/ExifViewerNode';
-import GoogleDriveNode from '@/node/ComplexNodes/GoogleDriveNode';
-import GpsMapNode from '@/node/ComplexNodes/GpsMapNode';
-import GpsSplitNode from '@/node/ComplexNodes/GpsSplitNode';
-import GrouperNode from '@/node/ComplexNodes/GrouperNode';
-import HotFolderReadNode from '@/node/ComplexNodes/HotFolderReadNode';
-import HotFolderWriteNode from '@/node/ComplexNodes/HotFolderWriteNode';
-import ImagePickerNode from '@/node/ComplexNodes/ImagePickerNode';
-import InformationNode from '@/node/ComplexNodes/InformationNode';
-import LutNode from '@/node/ComplexNodes/LutNode';
-import MergeChannelsNode from '@/node/ComplexNodes/MergeChannelsNode';
-import PhotoHistogramNode from '@/node/ComplexNodes/PhotoHistogramNode';
-import RescaleNode from '@/node/ComplexNodes/RescaleNode';
-import SelectedPhotoNode from '@/node/ComplexNodes/SelectedPhotoNode';
-import SinglePhotoViewerNode from '@/node/ComplexNodes/SinglePhotoViewerNode';
-import SourceNode from '@/node/ComplexNodes/SourceNode';
-import SplitChannelsNode from '@/node/ComplexNodes/SplitChannelsNode';
-import SplitToningNode from '@/node/ComplexNodes/SplitToningNode';
-import ViewerNode from '@/node/ComplexNodes/ViewerNode';
-import VignetteNode from '@/node/ComplexNodes/VignetteNode';
 import {
   BlackAndWhiteNode, BrightnessNode, ContrastNode, CropNode, ExposureNode,
   FadeNode, FlipNode, GammaNode, GrainNode, HdrNode, HighlightsNode,
   HueRotationNode, InvertNode, LuminosityNode, MirrorNode, PerspectiveNode, PopNode,
   RgbBlackPointNode, RgbMidtonesNode, RgbWhitePointNode, RotateNode, SaturationNode,
   SepiaNode, ShadowsNode, SharpenNode, TemperatureTintNode, VibranceNode, WhitesBlacksNode,
-} from '@/node/SimpleToggles';
+} from '@/pipeline/nodes';
+import AIAsyncColorizerNode from '@/pipeline/nodes/ComplexNodes/AIAsyncColorizerNode';
+import AIAsyncDenoiserNode from '@/pipeline/nodes/ComplexNodes/AIAsyncDenoiserNode';
+import AIPhotoEditorNode from '@/pipeline/nodes/ComplexNodes/AIPhotoEditorNode';
+import ArraySetOperationNode from '@/pipeline/nodes/ComplexNodes/ArraySetOperationNode';
+import ArraySwitchNode from '@/pipeline/nodes/ComplexNodes/ArraySwitchNode';
+import AskAINode from '@/pipeline/nodes/ComplexNodes/AskAINode';
+import CollageNode from '@/pipeline/nodes/ComplexNodes/CollageNode';
+import ExifSplitNode from '@/pipeline/nodes/ComplexNodes/ExifSplitNode';
+import ExifViewerNode from '@/pipeline/nodes/ComplexNodes/ExifViewerNode';
+import GoogleDriveNode from '@/pipeline/nodes/ComplexNodes/GoogleDriveNode';
+import GpsMapNode from '@/pipeline/nodes/ComplexNodes/GpsMapNode';
+import GpsSplitNode from '@/pipeline/nodes/ComplexNodes/GpsSplitNode';
+import GrouperNode from '@/pipeline/nodes/ComplexNodes/GrouperNode';
+import HotFolderReadNode from '@/pipeline/nodes/ComplexNodes/HotFolderReadNode';
+import HotFolderWriteNode from '@/pipeline/nodes/ComplexNodes/HotFolderWriteNode';
+import ImagePickerNode from '@/pipeline/nodes/ComplexNodes/ImagePickerNode';
+import InformationNode from '@/pipeline/nodes/ComplexNodes/InformationNode';
+import LutNode from '@/pipeline/nodes/ComplexNodes/LutNode';
+import MergeChannelsNode from '@/pipeline/nodes/ComplexNodes/MergeChannelsNode';
+import PhotoHistogramNode from '@/pipeline/nodes/ComplexNodes/PhotoHistogramNode';
+import RescaleNode from '@/pipeline/nodes/ComplexNodes/RescaleNode';
+import SelectedPhotoNode from '@/pipeline/nodes/ComplexNodes/SelectedPhotoNode';
+import SinglePhotoViewerNode from '@/pipeline/nodes/ComplexNodes/SinglePhotoViewerNode';
+import SourceNode from '@/pipeline/nodes/ComplexNodes/SourceNode';
+import SplitChannelsNode from '@/pipeline/nodes/ComplexNodes/SplitChannelsNode';
+import SplitToningNode from '@/pipeline/nodes/ComplexNodes/SplitToningNode';
+import ViewerNode from '@/pipeline/nodes/ComplexNodes/ViewerNode';
+import VignetteNode from '@/pipeline/nodes/ComplexNodes/VignetteNode';
 
 export const CONNECTION_LINE_TYPE = ConnectionLineType.SmoothStep;
 export const INITIAL_NODES: Node[] = [];
