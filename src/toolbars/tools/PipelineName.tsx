deaa52a9e190399c29464deaf14509316488f458
practiseo <practiseo@gmail.com>
2026-09-19 14:59:41 +0200

    refactor: replace TextField with InputBase for improved input handling in PipelineName component

--- src/toolbars/tools/PipelineName.tsx
+++ src/toolbars/tools/PipelineName.tsx
@@ -1,22 +1,23 @@
 import { usePipelineStore } from '@/context/pipelineStore';
-import { TextField } from '@mui/material';
+import { InputBase } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
 export default function PipelineName() {
   const { t } = useTranslation();
   const { currentPipeline, setCurrentPipelineName, setCurrentPipelineDirty } = usePipelineStore();
 
-  return <>
-    <TextField
-      id="pipeline-name"
-      size="small"
-      value={currentPipeline.name}
-      placeholder={t('pipelineTitlePlaceholder')}
-      onChange={(event) => {
-        setCurrentPipelineName(event.target.value);
-        setCurrentPipelineDirty(true);
-      }}
-      sx={{ maxWidth: 400, minWidth: 300 }}
-    />
-  </>
+  return <InputBase
+    id="pipeline-name"
+    value={currentPipeline.name}
+    placeholder={t('pipelineTitlePlaceholder')}
+    onChange={(event) => {
+      setCurrentPipelineName(event.target.value);
+      setCurrentPipelineDirty(true);
+    }}
+    sx={{ maxWidth: 400, minWidth: 300, px: 1,
+      '& .MuiInputBase-input': {
+        animation: 'none',
+      },
+    }}
+  />;
 }
